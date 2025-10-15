b7fb2311e1ca7729151056050a8e70594509c64a
siembra <77788776+siembra1978@users.noreply.github.com>
2025-10-15 10:54:05 -0400

    experimenting

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -19,15 +19,22 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: 'Train',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="tram" color={color} />,
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Bus',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="bus" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="streetcar"
+        options={{
+          title: 'Streetcar',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="logo.playstation" color={color} />,
         }}
       />
     </Tabs>
--- app/(tabs)/streetcar.tsx
+++ app/(tabs)/streetcar.tsx
@@ -6,7 +6,7 @@ import ParallaxScrollView from '@/components/parallax-scroll-view';
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerBackgroundColor={{ light: '#A1CEDC', dark: '#FF0000' }}
       headerImage={
         <Image
           source={require('@/assets/images/partial-react-logo.png')}
