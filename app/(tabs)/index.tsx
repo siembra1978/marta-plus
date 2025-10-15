d14ca16cdb1a3d70a3553b02539f9128ef377ca0
siembra <77788776+siembra1978@users.noreply.github.com>
2025-10-15 11:11:50 -0400

    gv

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,19 +1,8 @@
-import { Image } from 'expo-image';
 import { StyleSheet } from 'react-native';
-
-import ParallaxScrollView from '@/components/parallax-scroll-view';
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-    </ParallaxScrollView>
+    <></>
   );
 }
 
