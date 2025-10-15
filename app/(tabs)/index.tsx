ac2a7a11e4e70b2df31d6782a0c99cc81f5e37c3
siembra <77788776+siembra1978@users.noreply.github.com>
2025-10-14 23:02:49 -0400

    first test

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -18,7 +18,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">This is siembra.</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
